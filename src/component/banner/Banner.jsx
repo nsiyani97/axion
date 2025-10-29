15e68033f6e015dc94e6393dec18880a61125081
Nikhil Siyani <168623119+nsiyani97@users.noreply.github.com>
2025-10-29 17:16:51 +0530

    banner button solved

--- src/component/banner/Banner.jsx
+++ src/component/banner/Banner.jsx
@@ -5,6 +5,7 @@ import { Carousel, Container, Row, Col, Stack } from "react-bootstrap";
 import web from "./assets/web.svg";
 import mobile from "./assets/mobile.svg";
 import data from "./assets/data.svg";
+import { Image } from 'react-bootstrap';
 
 const images = {
   "web.svg": web,
@@ -15,21 +16,21 @@ const images = {
 function Banner() {
   return (
     <Container>
-      <Carousel indicators={true} interval={3000}>
+      <Carousel indicators={true} interval={1000}>
 
         {bannerData.map((item) => (
           <Carousel.Item key={item.id}>
             <Container>
               <Row className="align-items-center m-4">
-                <Col>
+                <Col xs={{order:2}} lg={{order:1}}>
                   <Stack gap={3}>
                     <div className="fs-1 semibold">{item.deskTitle}</div>
                     <div>{item.deskDisc}</div>
                     <div className="Btn regular">{item.deskBtn}</div>
                   </Stack>
                 </Col>
-                <Col>
-                  <img src={images[item.img]} alt="web" width={480} className="ps-4" />
+                <Col xs={{order:1}} lg={{order:2}}>
+                  <Image  src={images[item.img]} alt="web" fluid style={{ minWidth: "300px", maxWidth: "480px" }} />
                 </Col>
               </Row>
             </Container>
